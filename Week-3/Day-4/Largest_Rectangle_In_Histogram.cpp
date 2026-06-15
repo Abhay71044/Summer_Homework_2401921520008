6d393aff68677524d84425cfcc37a6f5edab5fe7
Abhay <abhaysingh71044@gmail.com>
2026-06-15 14:13:25 +0530

    Completed Week 3 Day 4

---
+++ Week-3/Day-4/Largest_Rectangle_In_Histogram.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    vector<int> nextSmallerElement(vector<int>& s){
+    vector<int>ans(s.size());
+    stack<int>st;
+    st.push(-1);
+    for(int i=s.size()-1;i>=0;i--){
+        int curr=s[i];
+        while(st.top()!=-1 && s[st.top()]>=curr){
+        st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+    }
+    return ans;
+    }
+
+    vector<int> prevSmallerElement(vector<int>s){
+    vector<int>ans(s.size());
+    stack<int>st;
+    st.push(-1);
+    for(int i=0;i<s.size();i++){
+        int curr=s[i];
+        while(st.top()!=-1 && s[st.top()]>=curr){
+        st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+    }
+    return ans;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int>prev=prevSmallerElement(heights);
+        vector<int>next=nextSmallerElement(heights);
+        int maxarea=INT_MIN;
+        int size=heights.size();
+
+        for(int i=0;i<heights.size();i++){
+            int length=heights[i];
+            if(next[i]==-1){
+            next[i]=size;
+            }
+            int width=next[i]-prev[i]-1;
+            int area=length*width;
+            maxarea=max(maxarea,area);
+
+        }
+        return maxarea;
+    }
+};
---
+++ Week-3/Day-4/Readme.md
@@ -0,0 +1,37 @@
+# Week 3 - Day 4
+
+Today I solved 3 Stack-based problems from LeetCode.
+
+## Problems Solved
+
+### 1. Daily Temperatures
+- Used a monotonic stack to keep track of unresolved temperatures.
+- Calculated the number of days required to get a warmer temperature.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+### 2. Evaluate Reverse Polish Notation
+- Used a stack to evaluate postfix expressions.
+- Performed arithmetic operations whenever an operator was encountered.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+### 3. Largest Rectangle in Histogram
+- Used Previous Smaller Element and Next Smaller Element concepts.
+- Calculated the maximum rectangular area for each bar.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+## Concepts Learned
+- Stack
+- Monotonic Stack
+- Previous Smaller Element
+- Next Smaller Element
+- Histogram Problems
+- Expression Evaluation
+
+## Author
+Abhay
